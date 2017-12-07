8858f1cd4fdd8e74699afcec0e6bb398d01e6034
Sergey Kozak <svkozak@icloud.com>
2017-12-06 20:52:06 -0500

    Implemented clear button

--- MinimalList/MinimalListTableViewController.swift
+++ MinimalList/MinimalListTableViewController.swift
@@ -1,8 +1,13 @@
 //
 //  MinimalListTableViewController.swift
-//  MinimalList
+//
+//    MinimalList
 //
 //  Created by Sergey Kozak on 06/12/2017.
+//  Team members:
+//  Irvinder Kaur
+//  Sergey Kozak
+//  Sergey Sharipov
 //  Copyright © 2017 Centennial. All rights reserved.
 //
 
@@ -17,6 +22,11 @@ class MinimalListTableViewController: UITableViewController {
     @IBOutlet weak var fifthLabel: UILabel!
     
     @IBOutlet weak var listTitle: UITextField!
+    @IBOutlet weak var firstTextField: UITextField!
+    @IBOutlet weak var secondTextField: UITextField!
+    @IBOutlet weak var thirdTextField: UITextField!
+    @IBOutlet weak var fourthTextField: UITextField!
+    @IBOutlet weak var fifthTextField: UITextField!
     
     
 
@@ -25,11 +35,7 @@ class MinimalListTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
 
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
     override func didReceiveMemoryWarning() {
@@ -44,7 +50,19 @@ class MinimalListTableViewController: UITableViewController {
   
     @IBAction func clearButton(_ sender: UIButton) {
         
+        listTitle.text = ""
         
+        firstTextField.text = ""
+        secondTextField.text = ""
+        thirdTextField.text = ""
+        fourthTextField.text = ""
+        fifthTextField.text = ""
+        
+        firstLabel.text = "0"
+        secondLabel.text = "0"
+        thirdLabel.text = "0"
+        fourthLabel.text = "0"
+        fifthLabel.text = "0"
     }
     
     
