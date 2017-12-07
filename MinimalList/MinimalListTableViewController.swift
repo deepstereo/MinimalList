28b9149bdcadfa2154fbfa916399a80ace0c22d0
Sergey Kozak <svkozak@icloud.com>
2017-12-06 21:06:54 -0500

    Added stepper functionality and comments

--- MinimalList/MinimalListTableViewController.swift
+++ MinimalList/MinimalListTableViewController.swift
@@ -1,13 +1,16 @@
 //
 //  MinimalListTableViewController.swift
 //
-//    MinimalList
+//  MinimalList
+//
+//  A minimal shopping list designed to keep you within budget
 //
 //  Created by Sergey Kozak on 06/12/2017.
 //  Team members:
-//  Irvinder Kaur
-//  Sergey Kozak
-//  Sergey Sharipov
+//  Irvinder Kaur 300929258
+//  Sergey Kozak 300979113
+//  Sergey Sharipov 300961984
+//
 //  Copyright © 2017 Centennial. All rights reserved.
 //
 
@@ -15,11 +18,16 @@ import UIKit
 
 class MinimalListTableViewController: UITableViewController {
     
+    // Outlets for text labels
+    
     @IBOutlet weak var firstLabel: UILabel!
     @IBOutlet weak var secondLabel: UILabel!
     @IBOutlet weak var thirdLabel: UILabel!
     @IBOutlet weak var fourthLabel: UILabel!
     @IBOutlet weak var fifthLabel: UILabel!
+    
+    
+    // Outlets for editable text fields
     
     @IBOutlet weak var listTitle: UITextField!
     @IBOutlet weak var firstTextField: UITextField!
@@ -29,35 +37,29 @@ class MinimalListTableViewController: UITableViewController {
     @IBOutlet weak var fifthTextField: UITextField!
     
     
-
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
 
 
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+ 
     
-    @IBAction func firstStepper(_ sender: UIStepper) {
-        
-    }
+    // Action to clear everything
     
-  
     @IBAction func clearButton(_ sender: UIButton) {
         
+        // title
         listTitle.text = ""
         
+        // shopping items
         firstTextField.text = ""
         secondTextField.text = ""
         thirdTextField.text = ""
         fourthTextField.text = ""
         fifthTextField.text = ""
         
+        // quantities
         firstLabel.text = "0"
         secondLabel.text = "0"
         thirdLabel.text = "0"
@@ -66,81 +68,30 @@ class MinimalListTableViewController: UITableViewController {
     }
     
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-
-    // MARK: - Table view data source
-
-//    override func numberOfSections(in tableView: UITableView) -> Int {
-//        // #warning Incomplete implementation, return the number of sections
-//        return 0
-//    }
-
-//    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        // #warning Incomplete implementation, return the number of rows
-//        return 0
-//    }
-
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
+    @IBAction func firstStepper(_ sender: UIStepper) {
+        switch sender.tag {
+        case 1:
+            firstLabel.text = Int(sender.value).description
+        case 2:
+            secondLabel.text = Int(sender.value).description
+        case 3:
+            thirdLabel.text = Int(sender.value).description
+        case 4:
+            fourthLabel.text = Int(sender.value).description
+        case 5:
+            fifthLabel.text = Int(sender.value).description
+        default:
+            firstLabel.text=""
+        }
+        
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
-    */
+    
 
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
